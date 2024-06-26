3b0084e2a6bc41c7db405d8fc75c84e5a6d8abf1
Emiliano Perez <emilianoperez.997@gmail.com>
2024-06-26 12:49:24 -0300

    [ADD] Clase 17

--- clase_17/hands_on_labs/src/routes/view.routes.js
+++ clase_17/hands_on_labs/src/routes/view.routes.js
@@ -9,7 +9,34 @@ router.get("/", async (req, res) => {
   try {
     const students = await studentModel.paginate({}, { limit, page });
     // console.log(students);
+
+    // const response = {
+    //   students,
+    //   nextLink: "/?page=" + (page + 1) + "&limit=" + limit,
+    //   prevLink: "/?page=" + (page - 1) + "&limit=" + limit,
+    // };
+
     res.render("students", { students });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+// Ejemplo endpoint /api/products
+router.get("/students", async (req, res) => {
+  const { page, limit, query } = req.query;
+
+  try {
+    const students = await studentModel.paginate(
+      { group: query },
+      { limit, page }
+    );
+    // console.log(students);
+
+    res.json({
+      status: "success",
+      ...students,
+    });
   } catch (error) {
     res.status(500).json({ error });
   }
